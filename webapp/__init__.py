685bf28978332b4091696a42576f5068a4e67258
dimaaaschich <dimaaaschich@yandex.ru>
2025-07-01 17:37:13 +0300

    Update __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
-from webapp.db import db
+def create_app():
 
 def create_app():
     app = Flask(__name__)
