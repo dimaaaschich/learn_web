059dfa5c8b902a3df8262e444a4cf36521aa14c2
Dmitriy Shishko <dimaaaschich@gmail.com>
2025-07-05 10:37:33 +0300

    Поддержка БД Flask-SQLalchemy

--- create_db.py
+++ create_db.py
@@ -1,11 +1,3 @@
-from webapp import db,  create_app
+from webapp import db, create_app
 
 db.create_all(app=create_app())
-    
-    # from webapp import create_app
-# from webapp.db import db
-
-# app = create_app()
-
-# with app.app_context():
-#     db.create_all()
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
-from webapp.model import db
+from webapp.db import db
 
 def create_app():
     app = Flask(__name__)
